49554e0ea1660b0ffc8054c0edc15677f6b28f0c
sharda maurya <shardamaurya42570@gmail.com>
2023-09-26 19:40:41 +0530

    updated start.sh file

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 # Add the directory to the PATH
 export PATH="/opt/render/.local/bin:$PATH"
 
+# Upgrade pip and install packages from requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
 # Start your Flask application
-python app.py
+python your_app.py
